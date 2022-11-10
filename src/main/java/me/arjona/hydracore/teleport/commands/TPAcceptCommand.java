3b523b21920692827e2234bd763be5ef1a03fcfc
TulioTriste <lunaloboez@gmail.com>
2022-11-09 23:29:56 -0300

    fixed tpaccept in the same server

--- src/main/java/me/arjona/hydracore/teleport/commands/TPAcceptCommand.java
+++ src/main/java/me/arjona/hydracore/teleport/commands/TPAcceptCommand.java
@@ -10,8 +10,10 @@ import me.arjona.hydracore.utilities.commands.Command;
 import me.arjona.hydracore.utilities.commands.CommandArgs;
 import me.arjona.hydracore.utilities.redis.impl.Payload;
 import me.arjona.hydracore.utilities.redis.util.RedisMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 /*
@@ -30,17 +32,23 @@ public class TPAcceptCommand extends BaseCommand {
 
         TPInfo tpInfo = Core.get().getTeleportManager().getCachePetition().getIfPresent(player.getUniqueId());
         if (tpInfo != null) {
-            Core.get().getRedisManager().write(new RedisMessage(Payload.TPA_ACCEPT_REPLICA)
-                    .setParam("SENDERNAME", tpInfo.getSenderName())
-                    .setParam("SENDERUUID", tpInfo.getSenderUUID().toString())
-                    .setParam("TARGETNAME", tpInfo.getTargetName())
-                    .setParam("TARGETUUID", tpInfo.getTargetUUID().toString())
-                    .setParam("SERVER", tpInfo.getServer())
-                    .setParam("TARGETSV", Core.get().getServerName())
-                    .toJSON());
-
             Core.get().getTeleportManager().getCacheAccept().put(tpInfo.getSenderUUID(),
                     new TPInfo(tpInfo.getSenderName(), tpInfo.getSenderUUID(), player.getName(), player.getUniqueId(), Core.get().getServerName()));
+
+            Player sender = Bukkit.getPlayer(tpInfo.getSenderUUID());
+            if (sender != null) {
+                sender.sendMessage(CC.translate("&aThe teleport request has been accepted"));
+                player.teleport(sender.getLocation());
+            } else {
+                Core.get().getRedisManager().write(new RedisMessage(Payload.TPA_ACCEPT_REPLICA)
+                        .setParam("SENDERNAME", tpInfo.getSenderName())
+                        .setParam("SENDERUUID", tpInfo.getSenderUUID().toString())
+                        .setParam("TARGETNAME", tpInfo.getTargetName())
+                        .setParam("TARGETUUID", tpInfo.getTargetUUID().toString())
+                        .setParam("SERVER", tpInfo.getServer())
+                        .setParam("TARGETSV", Core.get().getServerName())
+                        .toJSON());
+            }
 
             player.sendMessage(CC.translate("&aYou have accepted the teleport request."));
         } else {
